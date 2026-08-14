d81d629a84189882b3b66d3e86bf82e34e8feac0
lonvarek-afk <lonvarek@gmail.com>
2026-08-14 14:00:38 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,32 +1,21 @@
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-def get_main_keyboard():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+def main_keyboard():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = KeyboardButton("Ask Anything")
+    btn2 = KeyboardButton("Choose Language")
+    btn3 = KeyboardButton("History")
+    btn4 = KeyboardButton("Introduction")
     
-    # Safka 1-aad: Ask anything
-    btn_ask = KeyboardButton("🤖 Ask Anything")
-    
-    # Safka 2-aad: History & Choose language
-    btn_history = KeyboardButton("📜 History")
-    btn_lang = KeyboardButton("🌐 Choose Language")
-    
-    # Safka 3-aad: Introduction
-    btn_intro = KeyboardButton("ℹ️ Introduction")
-    
-    markup.row(btn_ask)
-    markup.row(btn_history, btn_lang)
-    markup.row(btn_intro)
-    
+    # 2 Midig jira iyo 2 Bidix jira (Grid 2x2)
+    markup.add(btn1, btn2)
+    markup.add(btn3, btn4)
     return markup
 
-def get_language_inline_keyboard():
-    markup = InlineKeyboardMarkup()
-    btn_en = InlineKeyboardButton("🇬🇧 English", callback_data="lang_en")
-    btn_so = InlineKeyboardButton("🇸🇴 Somali", callback_data="lang_so")
-    btn_ar = InlineKeyboardButton("🇸🇦 العربية", callback_data="lang_ar")
-    
-    markup.row(btn_en)
-    markup.row(btn_so)
-    markup.row(btn_ar)
-    
+def language_keyboard():
+    markup = InlineKeyboardMarkup(row_width=2)
+    btn_so = InlineKeyboardButton("Somali", callback_data="lang_so")
+    btn_en = InlineKeyboardButton("English", callback_data="lang_en")
+    btn_ar = InlineKeyboardButton("Arabic", callback_data="lang_ar")
+    markup.add(btn_so, btn_en, btn_ar)
     return markup
